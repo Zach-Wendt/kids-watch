2d2d2eebd9080d228091bd20a6debea09bcbebb3
Zach-Wendt <zachary.k.wendt-maldonado@outlook.com>
2022-10-05 00:06:01 -0700

    Prettier formatting
    Changes:
    - Ran formatting on files
    
    - Note: we need to find a good image as backdrop and logos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 // rafce to generate a functional component
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
@@ -11,38 +11,38 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await logOut();
-      navigate('/');
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <Link to='/'>
-        <h1 className='text-blue-600 text-5xl font-bold cursor-pointer'>
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        <h1 className="text-blue-600 text-5xl font-bold cursor-pointer">
           Kid's Watch
         </h1>
       </Link>
       {user?.email ? (
         <div>
-          <Link to='/account'>
-            <button className='text-white pr-4'>Account</button>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
           </Link>
           <button
             onClick={handleLogout}
-            className='bg-blue-600 px-6 py-2 rounded cursor-pointer text-white'
+            className="bg-blue-600 px-6 py-2 rounded cursor-pointer text-white"
           >
             Logout
           </button>
         </div>
       ) : (
         <div>
-          <Link to='/login'>
-            <button className='text-white pr-4'>Sign In</button>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
           </Link>
-          <Link to='/signup'>
-            <button className='bg-blue-600 px-6 py-2 rounded cursor-pointer text-white'>
+          <Link to="/signup">
+            <button className="bg-blue-600 px-6 py-2 rounded cursor-pointer text-white">
               Sign Up
             </button>
           </Link>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { UserAuth } from '../context/AuthContext';
+import { UserAuth } from "../context/AuthContext";
 
 // Define login functions variables
 const Login = () => {
@@ -36,12 +36,9 @@ const Login = () => {
       <div className="fixed w-full px-4 py-24 z-50">
         <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
           <div className="max-w-[320px] mx-auto py-16">
-            
-            
             <h1 className="text-3xl font-bold">Sign In</h1>
             {error ? <p className="p-3 bg-red-400 my-2">{error}</p> : null}
-            
-            
+
             <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
                 onChange={(e) => setEmail(e.target.value)}
@@ -57,8 +54,7 @@ const Login = () => {
                 placeholder="Password"
                 autoComplete="current-password"
               />
-              
-              
+
               <button className="bg-blue-600 py-3 my-6 rounded font-bold">
                 Sign In
               </button>
@@ -67,13 +63,10 @@ const Login = () => {
                   <input className="mr-2" type="checkbox" />
                   Remember me
                 </p>
-                
-                
+
                 <p>Need Help?</p>
               </div>
               <p className="py-8">
-                
-                
                 <span className="text-white-600">New to Kid's Watch?</span>{" "}
                 <Link to="/signup">Sign Up</Link>
               </p>
