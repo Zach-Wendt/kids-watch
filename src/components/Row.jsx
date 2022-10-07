e0fbd3ac00d16a0840860b08dcf6f6b1c9039b81
Zach-Wendt <zachary.k.wendt-maldonado@outlook.com>
2022-10-06 22:46:02 -0700

    Trailer work

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,9 +2,12 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -21,6 +24,27 @@ const Row = ({ title, fetchURL, rowID }) => {
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (item) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(item?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch(() => console.log("Temporarily Unavailable"));
+    }
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
@@ -35,9 +59,11 @@ const Row = ({ title, fetchURL, rowID }) => {
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
           {movies.map((item, id) => (
-            <Movie key={id} item={item} />
+            <Movie key={id} item={item} 
+            onClick = {handleClick(item)} />
           ))}
         </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         <MdChevronRight
           onClick={slideRight}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
