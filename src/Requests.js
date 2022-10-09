2047aa78d268ad0c75b244ee3ad58787fc2bfa63
Dame <dameo211@gmail.com>
2022-10-09 18:55:07 +0000

    Update Requests.js and firebase.js

--- src/Requests.js
+++ src/Requests.js
@@ -1,11 +1,11 @@
-const key = 'API'
+const key = 'c73cc0e048e3b1e4c09dfa43c5fb77fd';
 
 const requests = {
-  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
-  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1&certification_country=US&certification=G&include_adult=false`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1&&certification_country=US&certification=G&include_adult=false`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2&&certification_country=US&certification=G&include_adult=false`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&&certification_country=US&certification=G&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1&certification_country=US&certification=G&include_adult=false`,
 };
 
 export default requests;
